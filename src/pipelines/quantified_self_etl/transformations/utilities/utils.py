2521f813e23d18968b281a4d995d53b78dd1d69c
Punit Singh <punit98@protonmail.com>
2026-06-07 23:37:46 +0100

    typo

--- src/pipelines/quantified_self_etl/transformations/utilities/utils.py
+++ src/pipelines/quantified_self_etl/transformations/utilities/utils.py
@@ -7,7 +7,7 @@ from pyspark.sql import DataFrame
 
 def convert_column_values_to_lower_case(dataframe: DataFrame, columns_list: list[str]) -> DataFrame:
     """
-    Convers the values in the supplied columns to lower case.
+    Converts the values in the supplied columns to lower case.
     
     Args:
         dataframe (DataFrame): The dataframe.

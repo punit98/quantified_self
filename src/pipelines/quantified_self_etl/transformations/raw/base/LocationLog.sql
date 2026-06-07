39a0b5947d56572877292a1d7dea51bb4d84e376
Punit Singh <74264148+punit98@users.noreply.github.com>
2026-06-07 12:48:02 +0100

    Add raw tables from csvs (#3)

---
+++ src/pipelines/quantified_self_etl/transformations/raw/base/LocationLog.sql
@@ -0,0 +1,10 @@
+CREATE OR REFRESH STREAMING TABLE ${raw_schema}.LocationLog
+TBLPROPERTIES ('delta.columnMapping.mode' = 'name')
+AS SELECT *
+FROM STREAM read_files(
+  "/Volumes/${catalog}/${raw_schema}/landing_zone",
+  format => "csv",
+  header => "true",
+  inferSchema => "true",
+  fileNamePattern => "LocationLog.csv"
+)
---
+++ src/pipelines/quantified_self_etl/transformations/raw/base/WeatherLog.sql
@@ -0,0 +1,10 @@
+CREATE OR REFRESH STREAMING TABLE ${raw_schema}.WeatherLog
+TBLPROPERTIES ('delta.columnMapping.mode' = 'name')
+AS SELECT *
+FROM STREAM read_files(
+  "/Volumes/${catalog}/${raw_schema}/landing_zone",
+  format => "csv",
+  header => "true",
+  inferSchema => "true",
+  fileNamePattern => "WeatherLog.csv"
+)

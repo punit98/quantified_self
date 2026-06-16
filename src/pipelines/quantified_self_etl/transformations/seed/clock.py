2b3702e5473771b4a075711f524fc0fcaeb4f7da
Punit Singh <punit98@protonmail.com>
2026-06-16 21:37:09 +0100

    ruff

--- src/pipelines/quantified_self_etl/transformations/seed/clock.py
+++ src/pipelines/quantified_self_etl/transformations/seed/clock.py
@@ -1,37 +1,20 @@
 from pyspark import pipelines as dp
 from pyspark.sql import functions as sf
-from transformations.utilities import constants, utils, paths
+from transformations.utilities import paths
 
 
 @dp.temporary_view
 def hour():
     return (
-        spark.range(1)
-        .withColumn(
-            "hour",
-            sf.explode(
-                sf.sequence(
-                    sf.lit(0),
-                    sf.lit(23),
-                    sf.lit(1)
-                )
-            )
-        )
+        spark.range(1).withColumn("hour", sf.explode(sf.sequence(sf.lit(0), sf.lit(23), sf.lit(1))))
     ).drop("id")
+
 
 @dp.temporary_view
 def minutes():
     return (
-        spark.range(1)
-        .withColumn(
-            "minutes",
-            sf.explode(
-                sf.sequence(
-                    sf.lit(0),
-                    sf.lit(59),
-                    sf.lit(1)
-                )
-            )
+        spark.range(1).withColumn(
+            "minutes", sf.explode(sf.sequence(sf.lit(0), sf.lit(59), sf.lit(1)))
         )
     ).drop("id")
 
@@ -39,16 +22,8 @@ def minutes():
 @dp.temporary_view
 def seconds():
     return (
-        spark.range(1)
-        .withColumn(
-            "seconds",
-            sf.explode(
-                sf.sequence(
-                    sf.lit(0),
-                    sf.lit(59),
-                    sf.lit(1)
-                )
-            )
+        spark.range(1).withColumn(
+            "seconds", sf.explode(sf.sequence(sf.lit(0), sf.lit(59), sf.lit(1)))
         )
     ).drop("id")
 
@@ -56,8 +31,8 @@ def seconds():
 @dp.table(
     name=paths.CLOCK_PATH,
     comment="""
-    Clock table for all timestamps in the day 
-    """
+    Clock table for all timestamps in the day.
+    """,
 )
 def clock():
     hour_df = spark.read.table("hour")

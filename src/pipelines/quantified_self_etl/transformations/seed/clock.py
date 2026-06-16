568e6c351803e95f636776183dcd96bc84b75255
punit98 <punit98@protonmail.com>
2026-06-16 20:23:58 +0000

    hour

--- src/pipelines/quantified_self_etl/transformations/seed/clock.py
+++ src/pipelines/quantified_self_etl/transformations/seed/clock.py
@@ -3,31 +3,29 @@ from pyspark.sql import functions as sf
 from transformations.utilities import constants, utils, paths
 
 
-
-dp.temporary_view()
+@dp.temporary_view
 def hour():
-    hour = spark.range(1).withColumn(
-    "hour",
-    sf.explode(
-        sf.sequence(
-            sf.lit(0), 
-            sf.lit(23),
-            sf.lit(1)
+    return (
+        spark.range(1)
+        .withColumn(
+            "hour",
+            sf.explode(
+                sf.sequence(
+                    sf.lit(0),
+                    sf.lit(23),
+                    sf.lit(1)
+                )
+            )
         )
     )
-)
-
-
 
 
 @dp.table(
-    name = paths.CLOCK_PATH
-    COMMENT = 
-    """
+    name=paths.CLOCK_PATH,
+    comment="""
     Clock table for all timestamps in the day 
     """
 )
 def clock():
     hour_df = spark.read.table("hour")
-    clock = hour_df
-    return clock
+    return hour_df

b8dfab2fe91e0956e6c7fa671bf34833c2fa6eed
punit98 <punit98@protonmail.com>
2026-06-27 01:23:18 +0000

    add more calculated metrics

--- src/pipelines/quantified_self_etl/transformations/gold/workout_daily.py
+++ src/pipelines/quantified_self_etl/transformations/gold/workout_daily.py
@@ -1,6 +1,30 @@
 from pyspark import pipelines as dp
-from pyspark.sql import functions as sf
+from pyspark.sql import functions as sf, DataFrame
 from transformations.utilities import paths, utils
+
+
+def add_duration_columns(dataframe: DataFrame):
+    dataframe = dataframe.withColumn("hour_duration",
+        sf.timestamp_diff("HOUR", sf.col("start_timestamp"), sf.col("end_timestamp"))
+    )
+
+    dataframe = dataframe.withColumn("total_minute_duration",
+        sf.timestamp_diff("MINUTE", sf.col("start_timestamp"), sf.col("end_timestamp"))
+    )
+
+    dataframe = dataframe.withColumn("remainder_minutes",
+        (sf.col("total_minute_duration") - (sf.col("hour_duration") * 60))
+    )
+    return dataframe
+
+
+def add_intensity_metrics(dataframe: DataFrame):
+    dataframe = dataframe.withColumn(
+        "weight_per_rep",
+        sf.col("weight") / sf.col("total_reps"),
+    )
+    return dataframe
+
 
 
 @dp.table(
@@ -23,6 +47,7 @@ def workout_exercise_details():
         sf.max(sf.col("date_time")).alias("end_timestamp"),
         sf.count(sf.lit(1)).alias("number_of_sets"),
         sf.countDistinct(sf.col("muscle_group")).alias("number_of_muscle_groups"),
+        sf.mode(sf.col("muscle_group")).alias("primary_muscle_group"),
         sf.mean(sf.col("average_weight")).alias("average_weight"),
         sf.sum(sf.col("total_reps")).alias("number_of_reps"),
         sf.max(sf.col("weight")).alias("max_weight"),
@@ -41,8 +66,7 @@ def workout_exercise_details():
         "clock_key", "end_clock_key"
     )
 
-    workout_daily = workout_daily.withColumn("hour_duration",
-        sf.timestamp_diff("HOUR", sf.col("start_timestamp"), sf.col("end_timestamp"))
-    )
+    
+
 
     return workout_daily
